02d2e7f9bb583b7b5a4426829bc1d7527952d4ee
TygodeVries <zttdevries@gmail.com>
2024-03-01 19:48:47 +0100

    Added in basic commands

--- src/main/java/dev/thesheep/simpleresourcepack/api/ResoucepackCommand.java
+++ src/main/java/dev/thesheep/simpleresourcepack/api/ResoucepackCommand.java
@@ -2,22 +2,38 @@ package dev.thesheep.simpleresourcepack.api;
 
 import dev.thesheep.simpleresourcepack.SimpleResourcepack;
 import dev.thesheep.simpleresourcepack.file.Compressor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class ResoucepackCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NonNull CommandSender sender, @NonNull  Command command, @NonNull  String label, String[] args) {
 
         Configuration config = SimpleResourcepack.getInstance().getConfig();
 
         if(args.length > 0 && args[0].equalsIgnoreCase("forceupdate") && (sender.hasPermission("simple_resoucepack.force") || sender.isOp()))
         {
+            if(args.length == 1)
+            {
+                sender.sendMessage("You need to specify a player name");
+                return true;
+            }
+
+            Player player = Bukkit.getPlayer(args[1]);
+
+            if(player == null)
+            {
+                sender.sendMessage("Player not found");
+                return true;
+            }
+
             sender.sendMessage(config.getString("message_force", "Forcing player to update resoucepack..."));
-            Compressor.compressAll();
+            SimpleResourcepack.getInstance().sendDefaultPacks(player);
             return true;
         }
 
