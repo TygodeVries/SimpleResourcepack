df87be2fb87bc857f40ecba816e78ea6de9e780f
Supermarcel10 <marcelbarlik@gmail.com>
2024-07-08 00:50:14 +0100

    - Removed configurations in order to generate only one JAR file of combined versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -8,7 +6,7 @@ plugins {
 group = "dev.thesheep"
 version = "2.5"
 
-val versions = listOf("1_16" to "1.16.5", "1_20" to "1.20.4")
+val earliestSupportedAPIVersion = "1.16"
 
 repositories {
     mavenCentral()
@@ -16,48 +14,9 @@ repositories {
     maven("https://oss.sonatype.org/content/groups/public/")
 }
 
-configurations {
-    versions.forEach { (suffix, _) ->
-        create("v$suffix")
-    }
-}
-
 dependencies {
-    // General dependencies
     implementation("org.bstats:bstats-bukkit:3.0.2")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-
-    // Spigot API dependency (based on version)
-    versions.forEach { (suffix, version) ->
-        compileOnly("org.spigotmc:spigot-api:$version-R0.1-SNAPSHOT") {
-            configurations["v$suffix"]
-        }
-    }
-}
-
-versions.forEach { (suffix, _) ->
-    tasks.register<ShadowJar>("v${suffix}Jar") {
-        filteringCharset = "UTF-8"
-
-        // Add the version suffix to the jar file name
-        archiveClassifier.set("v$suffix")
-
-        // Shade the dependencies
-        configurations = listOf(project.configurations["v$suffix"], project.configurations["runtimeClasspath"])
-        from(sourceSets.main.get().output)
-        relocate("org.bstats", "dev.thesheep")
-
-        // Get API version from suffix
-        val apiVersion = suffix.replace("_", ".")
-
-        // Add the version and API version to the plugin.yml
-        filesMatching("**/plugin.yml") {
-            expand(
-                "version" to project.version,
-                "apiVersion" to apiVersion
-            )
-        }
-    }
+    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
 }
 
 tasks.withType<JavaCompile> {
@@ -67,10 +26,30 @@ tasks.withType<JavaCompile> {
 
 // Default build task depends on all versioned jar tasks
 tasks.named("build") {
-    dependsOn(versions.map { (suffix, _) -> "v${suffix}Jar" })
+    dependsOn(tasks.shadowJar)
 }
 
 // Disable the default jar task
 tasks.jar {
     enabled = false
+}
+
+tasks.shadowJar {
+    filteringCharset = "UTF-8"
+
+    // Remove the suffix from the jar file
+    archiveClassifier.set("")
+
+    // Shade the dependencies
+    configurations = listOf(project.configurations["runtimeClasspath"])
+    from(sourceSets.main.get().output)
+    relocate("org.bstats", "dev.thesheep")
+
+    // Add the version to the plugin.yml
+    filesMatching("**/plugin.yml") {
+        expand(
+            "version" to project.version,
+            "apiVersion" to earliestSupportedAPIVersion
+        )
+    }
 }
