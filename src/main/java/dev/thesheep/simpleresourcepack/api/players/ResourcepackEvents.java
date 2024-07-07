2a43ac4ba6e52d1bed8dceb829777cf1d6380256
Supermarcel10 <marcelbarlik@gmail.com>
2024-07-07 23:45:47 +0100

    - Added additional PlayerResourcePackStatusEvent for legacy declines while forced.

--- src/main/java/dev/thesheep/simpleresourcepack/api/players/ResourcepackEvents.java
+++ src/main/java/dev/thesheep/simpleresourcepack/api/players/ResourcepackEvents.java
@@ -2,6 +2,7 @@ package dev.thesheep.simpleresourcepack.api.players;
 
 import dev.thesheep.simpleresourcepack.SimpleResourcepack;
 import dev.thesheep.simpleresourcepack.legacy.ActionBar;
+import dev.thesheep.simpleresourcepack.legacy.ResourcePack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,6 +25,13 @@ public class ResourcepackEvents implements Listener {
         {
             String msg = SimpleResourcepack.getInstance().getConfig().getString("message_download_complete", "");
             ActionBar.sendActionBar(event.getPlayer(), msg);
+        }
+        else if (ResourcePack.isUsingLegacyMethod()
+                && event.getStatus() == PlayerResourcePackStatusEvent.Status.DECLINED
+                && ResourcePack.isForced(event.getPlayer()))
+        {
+            // TODO: Consider making this message configurable in config.yml (See if it's also possible to do the same for 1.20+ for consistency)
+            event.getPlayer().kickPlayer("You must accept the resource pack to play on this server.\nEnsure you set Edit Server > Server Resource Packs to Enabled");
         }
         else if(event.getStatus() == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD)
         {
